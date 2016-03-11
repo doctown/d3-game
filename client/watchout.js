339996558c4d3a9151d61f5c5cd3ec802f1393a4
David Ogor <doctown@yahoo.com>
2016-03-11 12:10:04 -0800

    Added svg image circle that move randomly every second.

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,84 @@
 // start slingin' some d3 here.
+// CREATE CIRCLES
+//var circle = d3.selectAll('circle');
+//circle.style('fill', 'green');
+//circle.data([32, 45, 76]);
+//
+//circle.attr('cx', function() {
+//  return Math.random() * 720;
+//}).attr('r', function (d) {
+//  return Math.sqrt(d);
+//});
+
+// Initial data for circles
+var data = [32, 57, 112, 293];
+
+// ADD SVG CIRCLES BASED ON AMOUNT OF DATA
+var svg = d3.select('svg');
+/*
+svg.selectAll('circle')
+  .data([32, 57, 112, 293])
+  .enter().append('circle')
+  .attr('cy', 60)
+  .attr('cx', function(d, i) { return i * 100 + 30; })
+  .attr('r', function(d) { return Math.sqrt(d); });
+
+*/
+
+
+/*
+svg.selectAll('circle')
+  .data(data)
+  .enter().append('circle')
+  .attr('cy', 70).attr('cx', function(d) {
+    return d + 3;
+  }).attr('r', function(d) {
+    return   Math.sqrt(d);
+  });
+*/
+
+// Update function for updating the location of the circles
+// Updates the location of the circles on the screen
+var update = function (data) {
+  // DATA JOIN
+  // Join new data with old data
+  var circle = svg.selectAll('circle')
+    .data(data);
+
+  // Update the circle's class css
+  circle.attr('class', 'update');
+
+  // ENTER
+  circle.enter().append('circle')
+    .attr('class', 'enter')
+    .attr('cx', function(d) {
+      return d * Math.random();
+    })
+    .attr('cy', 60)
+    .attr('r', function(d) {
+      return 10;
+    });
+
+
+  // UPDATE
+  circle.transition()
+    .attr('cx', function (d) {
+      return Math.random()  * d;
+    })
+    .attr('cy', function (d) {
+      return Math.random() * d;
+    });
+  // EXIT
+  // Remove old elements as needed
+  // circle.exit().remove();
+};
+
+// Call the initial update
+update(data);
+
+
+// Update the circles every 2 seconds
+setInterval(function () {
+  var newData = d3.shuffle(data);
+  update(data);
+}, 1000);
