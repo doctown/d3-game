fc6c01aaec71a2c7c2e6deb9d4cecd0a5c5f3f40
doctown <doctown@yahoo.com>
2016-03-11 16:13:13 -0800

    Main circle will drag across the screen based on mouse movements.

--- client/watchout.js
+++ client/watchout.js
@@ -1,41 +1,27 @@
-// start slingin' some d3 here.
-// CREATE CIRCLES
-//var circle = d3.selectAll('circle');
-//circle.style('fill', 'green');
-//circle.data([32, 45, 76]);
-//
-//circle.attr('cx', function() {
-//  return Math.random() * 720;
-//}).attr('r', function (d) {
-//  return Math.sqrt(d);
-//});
+
+var boardWidth = 720;
+var boardHeight = 720;
+
+var circleWidth = 10;
+var circleHeight = 10;
+
+// Moves the circle to new location based on drag events
+var dragmove = function (d, i) {
+  d3.select(this)
+      .attr('cx', d.x = Math.max(0, Math.min(boardWidth - circleWidth, d3.event.x)));
+  d3.select(this)
+      .attr('cy', d.y = Math.max(0, Math.min(boardHeight - circleHeight, d3.event.y)));
+};
+
+// Event handler for dragging circle on the board
+var dragNode = d3.behavior.drag()
+  .on('drag', dragmove);
 
 // Initial data for circles
-var data = [32, 57, 112, 293];
+var data = [boardHeight, boardHeight, boardHeight, boardHeight];
 
 // ADD SVG CIRCLES BASED ON AMOUNT OF DATA
 var svg = d3.select('svg');
-/*
-svg.selectAll('circle')
-  .data([32, 57, 112, 293])
-  .enter().append('circle')
-  .attr('cy', 60)
-  .attr('cx', function(d, i) { return i * 100 + 30; })
-  .attr('r', function(d) { return Math.sqrt(d); });
-
-*/
-
-
-/*
-svg.selectAll('circle')
-  .data(data)
-  .enter().append('circle')
-  .attr('cy', 70).attr('cx', function(d) {
-    return d + 3;
-  }).attr('r', function(d) {
-    return   Math.sqrt(d);
-  });
-*/
 
 // Update function for updating the location of the circles
 // Updates the location of the circles on the screen
@@ -47,6 +33,16 @@ var update = function (data) {
 
   // Update the circle's class css
   circle.attr('class', 'update');
+  // UPDATE
+  circle.transition()
+    .attr('cx', function (d) {
+      return Math.random() * d;
+    })
+    .attr('cy', function (d) {
+      return Math.random() * d;
+    });
+  
+  // EXIT
 
   // ENTER
   circle.enter().append('circle')
@@ -54,21 +50,9 @@ var update = function (data) {
     .attr('cx', function(d) {
       return d * Math.random();
     })
-    .attr('cy', 60)
-    .attr('r', function(d) {
-      return 10;
-    });
+    .attr('cy', function(d) { return d * Math.random(); })
+    .attr('r', 10);
 
-
-  // UPDATE
-  circle.transition()
-    .attr('cx', function (d) {
-      return Math.random()  * d;
-    })
-    .attr('cy', function (d) {
-      return Math.random() * d;
-    });
-  // EXIT
   // Remove old elements as needed
   // circle.exit().remove();
 };
@@ -76,11 +60,9 @@ var update = function (data) {
 // Call the initial update
 update(data);
 
-
-
 // MOVABLE CIRCLE
 // Get the circle that is updated by the mouse
-var mouse = d3.selectAll('svg');
+var mouse = d3.select('svg');
 
 movableCircle = mouse.selectAll('.mouse').data([1]);
 movableCircle.enter()
@@ -90,23 +72,11 @@ movableCircle.enter()
   .attr('cy', function (d) { return 50; })
   .attr('r', function (d) {
     return 10;
-  }).style('fill', 'green');
-
-svg.on('mousemove', function () {
-  var coordinates = [0, 0];
-  coordinates = d3.mouse(this);
-  var x = coordinates[0];
-  var y = coordinates[1];
-
-  movableCircle.transition()
-    .attr('cx', function (d) { return x; })
-    .attr('cy', function (d) { return y; });
-
-});
-
+  }).style('fill', 'green')
+  .call(dragNode);
 
 // Update the circles every 2 seconds
 setInterval(function () {
-  var newData = d3.shuffle(data);
+  //var newData = d3.shuffle(data);
   update(data);
 }, 1000);
